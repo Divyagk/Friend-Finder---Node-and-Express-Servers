62070fa09ceabc7fc34400ff2035bb3621f5f5bf
divya <gopalakrishnandivya6@gmail.com>
2019-01-27 17:59:08 -0700

    friend list added

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friends= [
+    {
+        "name": "Mohanlal",
+        "photo":"https://www.filmibeat.com/ph-big/2015/12/mohanlal_145035026860.jpg",
+        "scores": [5,3,3,4,2,4,1,4,5,5]
+    },
+    {
+        "name": "Fahadh Faazil",
+        "photo":"https://1847884116.rsc.cdn77.org/malayalam/gallery/actor/fahadfazil/fahadh_faazil_47.jpg",
+        "scores": []
+    },
+    {
+        "name": "",
+        "photo":"https://1847884116.rsc.cdn77.org/malayalam/gallery/actor/dulkarsalman/dulqursalman071218_14.jpg",
+        "scores": []
+    }
+]

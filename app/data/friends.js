cd37ab7461c5982c0152ae4deaa617ac5efe2df2
divya <gopalakrishnandivya6@gmail.com>
2019-01-27 23:25:44 -0700

    final commit

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,39 +1,21 @@
-// var friends= [
-//     {
-//         "name": "Mohanlal",
-//         "photo":"https://www.filmibeat.com/ph-big/2015/12/mohanlal_145035026860.jpg",
-//         "scores": [5,3,3,4,2,4,1,4,5,5]
-//     },
-//     {
-//         "name": "Fahadh Faazil",
-//         "photo":"https://1847884116.rsc.cdn77.org/malayalam/gallery/actor/fahadfazil/fahadh_faazil_47.jpg",
-//         "scores": []
-//     },
-//     {
-//         "name": "",
-//         "photo":"https://1847884116.rsc.cdn77.org/malayalam/gallery/actor/dulkarsalman/dulqursalman071218_14.jpg",
-//         "scores": []
-//     }
-// ]
-
-// module.exports = friends;
-var friendList = [
+var friendLists= [
     {
-      name: 'Stefanie Ding',
-      profilePic: 'https://avatars3.githubusercontent.com/u/17751317?v=3&s=466',
-      scores: [
-        "4",
-        "5",
-        "5",
-        "2",
-        "4",
-        "5",
-        "4",
-        "4",
-        "3",
-        "4"
-      ]
+        "name": "Mohanlal",
+        "photo":"https://www.filmibeat.com/ph-big/2015/12/mohanlal_145035026860.jpg",
+        "scores": [5,3,3,4,2,4,1,4,5,5]
+    },
+    {
+        "name": "Fahadh Faazil",
+        "photo":"https://1847884116.rsc.cdn77.org/malayalam/gallery/actor/fahadfazil/fahadh_faazil_47.jpg",
+        "scores": [4,3,1,2,5,3,4,4,4,4]
+    },
+    {
+        "name": "Dulquar",
+        "photo":"https://1847884116.rsc.cdn77.org/malayalam/gallery/actor/dulkarsalman/dulqursalman071218_14.jpg",
+        "scores": [1,3,4,4,4,5,3,2,1,5]
     }
-  ]
+]
+
+
   
   module.exports = friendList;
